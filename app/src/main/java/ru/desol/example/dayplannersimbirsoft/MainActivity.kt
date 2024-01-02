c4a59fc0c078777852ab0f94b3cb647b89bd41c0
Denis Solovyov <infinityx93@gmail.com>
2024-01-02 09:31:17 +0300

    init project

---
+++ app/src/main/java/ru/desol/example/dayplannersimbirsoft/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.desol.example.dayplannersimbirsoft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
